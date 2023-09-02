317f07b51a16c563b933284a62e3a468a797800f
2501jayanthi <2501jayanthi@gmail.com>
2023-09-02 16:29:02 +0000

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Job from "./components/Job";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import "./App.css";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/product" component={Product} />
+      <ProtectedRoute exact path="/job" component={Job} />
+      <ProtectedRoute exact path="/profile" component={Profile} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
